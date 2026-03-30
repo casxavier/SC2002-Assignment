09015c5ca1683a7ac52ac54d8c28a9f7b265cbf8
yoda22-coder <leeyuxiang2005@gmail.com>
2026-03-30 14:12:48 +0800

    Update Warrior.java

--- src/combatant/Warrior.java
+++ src/combatant/Warrior.java
@@ -5,4 +5,9 @@ public class Warrior extends Player {
     public Warrior(String name) {
         super(name, 260, 40, 20, 30);
     }
+
+    public void shieldBash(Enemy name) {
+        Enemy.name.hp -= Warrior.name.attack;
+        Enemy.actor.canAct() = 0;
+    }
 }
