23b2e7642545393bde6ea26bc0d1686bb7ff47d1
yoda22-coder <leeyuxiang2005@gmail.com>
2026-03-30 14:58:01 +0800

    Update Warrior.java

--- src/combatant/Warrior.java
+++ src/combatant/Warrior.java
@@ -7,7 +7,7 @@ public class Warrior extends Player {
     }
 
     public void shieldBash(Enemy enemy) {
-        enemy.hp -= this.attack;
+        enemy.takeDamage(this.attack);
         addStatusEffect(stun);    
     }
 }
