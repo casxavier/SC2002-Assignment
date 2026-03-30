59e38e11b1e8ffcd2221a1611290de26adbbce70
yoda22-coder <leeyuxiang2005@gmail.com>
2026-03-30 15:02:30 +0800

    Update Warrior.java

--- src/combatant/Warrior.java
+++ src/combatant/Warrior.java
@@ -5,9 +5,4 @@ public class Warrior extends Player {
     public Warrior(String name) {
         super(name, 260, 40, 20, 30);
     }
-
-    public void shieldBash(Enemy enemy) {
-        enemy.takeDamage(this.attack);
-        addStatusEffect(stun);    //stun for this and next rounds
-    }
 }
